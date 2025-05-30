26c05351e1e41ae059ec3da079b215ce910bcebd
Isha-Yasin886 <lisaisha890@gmail.com>
2025-05-30 12:12:44 +0500

    1 Day of JavaScript

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hey isha!");
+// with global
+// var age = 25;
+// if(true){
+// console.log(age);
+// }
+
+// with function
+// function solve(){
+//     var age = 23;
+//     console.log(age);
+// }
+// solve();
+// console.log(age);
+
+var x = 10;
+var x = 20;
+console.log(x);
