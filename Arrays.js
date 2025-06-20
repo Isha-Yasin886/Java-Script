bf96f846a4fab025d5c7a79468a550ce9f24877f
iramyaseen <199124444+Isha-Yasin886@users.noreply.github.com>
2025-06-20 19:06:58 +0500

    Learn Arrays

---
+++ Arrays.js
@@ -0,0 +1,14 @@
+let obj = {
+    name: "Isha Yasin",
+    age: 19,
+    study: "BSCS",
+    calculateSum: function() {
+        let a = 10;
+        let b = 20;
+        let ans = a + b;
+        console.log(ans);  
+    }
+};
+
+console.log(obj);      // shows the object
+obj.calculateSum();    // calls the function
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Learn and practice JS
+# Arrays in js .
+Hr program ko 2 types sy memory alert hoti ha 
+1). Stack memory in which all permative data types like int,float,etc store in stack memory
+2). Heap memory in which all reference type or all object,arrays store in it. 
